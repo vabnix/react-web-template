2f7e12e3df9161a3f5b34f61f327d13d01861449
Vaibhav Srivastava <vabnix@hotmail.com>
2017-07-02 23:10:19 -0500

    Introducing pages

---
+++ src/js/component/about/aboutPage.js
@@ -0,0 +1,17 @@
+/**
+ * Created by vaibhavsrivastava on 7/2/17.
+ */
+var React = require('react');
+
+var About = React.createClass({
+    render: function () {
+        return (
+            <div className="jumbotron">
+                <h1>About Page</h1>
+                <p>Welcome to About page</p>
+            </div>
+        );
+    }
+});
+
+module.exports = About;
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,39 @@
  * Created by vaibhavsrivastava on 7/1/17.
  */
 $ = jQuery = require('jquery');
-var App = console.log('Hello World');
 
-module.exports = App;
+var React = require('react');
+var ReactDom = require('react-dom');
+var Home = require('./component/home/homePage');
+var About = require('./component/about/aboutPage');
+var Header = require('./component/common/header');
+
+(function (win) {
+    "use strict";
+    var App = React.createClass({
+        render: function () {
+            var Child;
+            switch (this.props.route) {
+                case 'about':
+                    Child = About;
+                    break;
+                default:
+                    Child = Home;
+            }
+            return (
+                <div>
+                    <Child/>
+                </div>
+            );
+        }
+
+    });
+
+    function render() {
+        var route = window.location.hash.substr(1);
+        ReactDom.render(<App route={route}/>, document.getElementById('app'));
+    }
+
+    window.addEventListener('hashchange', render);
+    render();
+})(window);
